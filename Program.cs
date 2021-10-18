89d3bc82e5dbce14084faa5162f8dc9991d71095
Mateusz PIentka <pietka00@gmail.com>
2021-10-18 11:37:23 +0200

    First commit here

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace praktyki_apka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ile liczb fibonaciego chcesz uzyskać");
+            string liczba = Console.ReadLine();
+            Console.WriteLine("Ciąg fibonaciego:");
+            int x = 0;
+            int y = 1;
+            int wynik;
+            for (int i=0;i<Int16.Parse(liczba);i++){
+                wynik = x + y;
+                Console.WriteLine(wynik);
+                x = y;
+                y = wynik;
+            }
+        }
+    }
+}
